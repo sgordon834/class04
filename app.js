94fdc5c6d483dc5f6906bae3d549b7acc45ff94b
sgordon834 <sgordon834@gmail.com>
2017-02-08 23:19:51 -0800

    change number 1-20

--- app.js
+++ app.js
@@ -1,22 +1,6 @@
 //Script to obtain user input
 //Start Game
-<<<<<<< HEAD
-
-<<<<<<< HEAD
-=======
-
-// answerOne = question1();
-
-
-// console.log(userName);
-
-//Question 1
-
->>>>>>> 6398e452b6ede898a220b295da7c478616e8ed61
-function question1() {
-=======
 // start question1 function
->>>>>>> 965e9c94e67b21d89c75d34a60a13869e0e27b34
 var toScor = 0;
 var userName = prompt('what\'s is your name?');
 
@@ -76,16 +60,9 @@ function question1() {
 
   } else {
 
-<<<<<<< HEAD
-  alert('Oh no, that is incorrect.');
-}
-<<<<<<< HEAD
-=======
-=======
     alert('Oh no, that is incorrect.');
   }
   console.log(answerFour);
->>>>>>> 965e9c94e67b21d89c75d34a60a13869e0e27b34
 }
 
 //calling question1 function
@@ -94,189 +71,6 @@ question1();
 // Question 5
 //start questionTwo funciton
 function questionTwo() {
-<<<<<<< HEAD
-var whichLang = prompt('What is my favorite programming language?');
-var userAnswer = ['javascript', 'python', 'java', 'c'];
-
-if (userAnswer.indexOf(whichLang) > -1) {
-
-  toScor += 1;
-  alert('You\'re on your way to tech greatness!');
-
-} else if (userAnswer.indexOf(whichLang) === -1) {
-  alert('Learn to code or stay in yesteryear!');
-  console.log('User score is ' + toScor + 'so far.');
-
->>>>>>> 6398e452b6ede898a220b295da7c478616e8ed61
-}
-
-question1();
-
-// Question 5
-// var whichLang = prompt('What is my favorite programming language?');
-// var userAnswer = ['javascript', 'python', 'java', 'c'];
-//
-// if (userAnswer.indexOf(whichLang) > -1) {
-//
-//   toScor += 1;
-//   alert('You\'re on your way to tech greatness!');
-//
-// } else if (userAnswer.indexOf(whichLang) === -1) {
-//   alert('Learn to code or stay in yesteryear!');
-//   console.log('User score is ' + toScor + 'so far.');
-//
-// }
-// // Question 6
-// var answerOne = prompt('Do I own a Mac? (y or yes/n or no)').toLowerCase();
-// var myAnswerOne = 'yes';
-// var myAnswerTwo = 'y';
-//
-// if (answerOne === myAnswerOne || answerOne === myAnswerTwo) {
-//
-//   toScor += 1;
-//   alert('Whoopee! that is correct.');
-//
-//
-// } else {
-//
-//   alert('That\'s not correct, try again.');
-// }
-// // Question 7
-var ranNum = Math.floor(Math.random() * 20) + 1;
-var countAtt = 0;
-
-while (countAtt < 5) {
-  countAtt++;
-  var userNumb = prompt('Select a number between 1 - 20 (you have 4 guesses).');
-
-  if (parseInt(userNumb) === ranNum) {
-
-    toScor += 1;
-    alert('That\'s right!!!.');
-    break;
-
-  }
-  if (parseInt(userNumb) > ranNum) {
-    alert('That\'s too high, try again.');
-
-  }
-  if (parseInt(userNumb) < ranNum) {
-    alert('That\'s too low, try again.');
-
-  }
-  if (countAtt === 5) {
-    alert('Sorry that was the last guess.');
-  }
-}
-<<<<<<< HEAD
-console.log(ranNum);
-// // Question 8
-// var count = 0;
-// var arr = ['florida', 'new jersey', 'hawaii', 'alaska', 'virginia', 'arizona'];
-//
-// while (count < 6) {
-//   count++;
-//   var userGuess = prompt('Name a State I have lived in besides Washington. You have 6 tries.');
-//
-//   if(userGuess === 'florida') {
-//
-//     toScor += 1;
-//     for (i = 0; i < arr.length; i++)
-//     alert(arr[i]);
-//     break;
-//
-//   }if (userGuess === 'new jersey') {
-//
-//     toScor += 1;
-//     for (i = 0; i < arr.length; i++)
-//     alert(arr[i]);
-//     break;
-//
-//   }if (userGuess === 'hawaii') {
-//
-//     toScor += 1;
-//     for (i = 0; i < arr.length; i++)
-//     alert(arr[i]);
-//     break;
-//
-//   }if (userGuess === 'alaska') {
-//
-//     toScor += 1;
-//     for (i = 0; i < arr.length; i++)
-//     alert(arr[i]);
-//     break;
-//
-//   }if (userGuess === 'virginia') {
-//
-//     toScor += 1;
-//     for (i = 0; i < arr.length; i++)
-//     alert(arr[i]);
-//     break;
-//
-//   }
-//   if (userGuess === 'arizona') {
-//
-//     toScor += 1;
-//     for (i = 0; i < arr.length; i++)
-//     alert(arr[i]);
-//     break;
-//
-//   }if (count === 6) {
-//     for (i = 0; i < arr.length; i++)
-//     alert(arr[i]);
-//   }
-// }
-// console.log(arr[i]);
-// //Keep score and show user results
-// if (toScor === 8) {
-//
-//   alert('You got 8 of 8 correct, ' + userName + ' great going!');
-//
-// }if (toScor === 7) {
-//
-//   alert('You got 7 of 8 correct, ' + userName + ' strong effort!');
-//
-// }if (toScor === 6) {
-//
-//   alert('You got 6 of 8 correct, ' + userName + ' nice try.');
-//
-// }if (toScor === 5) {
-//
-//   alert('You got 5 of 8 correct, ' + userName + ' try again.');
-//
-// }if (toScor === 4) {
-//
-//   alert('You got 4 of 8 correct, ' + userName + ' try again.');
-//
-// }if (toScor === 3) {
-//
-//   alert('You got 3 of 8 correct, ' + userName + ' try again.');
-//
-// }if (toScor === 2) {
-//
-//   alert('You got 2 of 8 correct, ' + userName + ' try again.');
-//
-// }if (toScor === 1) {
-//
-//   alert('You got 1 of 8 correct, ' + userName + ' try again.');
-//
-// }if (toScor === 0) {
-//
-//   alert('You got 0 of 8 correct, ' + userName + ' try again.');
-// }
-// console.log(toScor);
-=======
-console.log(16);
-// Question 8
-var count = 0;
-var arr = ['florida', 'new jersey', 'hawaii', 'alaska', 'virginia', 'arizona'];
-
-while (count < 6) {
-  count++;
-  var userGuess = prompt('Name a State I have lived in besides Washington. You have 6 tries.');
-
-  if(userGuess === 'florida') {
-=======
   var whichLang = prompt('What is my favorite programming language?');
   var userAnswer = ['javascript', 'python', 'java', 'c'];
 
@@ -294,7 +88,6 @@ while (count < 6) {
   var answerOne = prompt('Do I own a Mac? (y or yes/n or no)').toLowerCase();
   var myAnswerOne = 'yes';
   var myAnswerTwo = 'y';
->>>>>>> 965e9c94e67b21d89c75d34a60a13869e0e27b34
 
   if (answerOne === myAnswerOne || answerOne === myAnswerTwo) {
 
@@ -312,7 +105,7 @@ while (count < 6) {
 
   while (countAtt < 4) {
     countAtt++;
-    var userNumb = prompt('Select a number between 1 - 25 (you have 4 guesses).');
+    var userNumb = prompt('Select a number between 1 - 20 (you have 4 guesses).');
 
     if (parseInt(userNumb) === ranNum) {
 
@@ -329,10 +122,10 @@ while (count < 6) {
       alert('That\'s too low, try again.');
 
     }
-    // if (countAtt === 3) {
-    //   alert('This is your last attempt!');
-    //
-    // }
+    if (countAtt === 3) {
+      alert('This is your last attempt!');
+
+    }
     if (countAtt === 4) {
       alert('Sorry that was the last guess.');
     }
@@ -448,10 +241,5 @@ while (count < 6) {
   //Keep score and show user results
   console.log(toScor);
 }
-<<<<<<< HEAD
-console.log(toScor);
->>>>>>> 6398e452b6ede898a220b295da7c478616e8ed61
-=======
 //call questionTwo function
 questionTwo();
->>>>>>> 965e9c94e67b21d89c75d34a60a13869e0e27b34
