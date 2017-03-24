6fe2379fee510bd1303837f395d8504769fe10f6
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2017-03-23 19:53:18 -0700

    wrapped set of prompts into one function

--- app.js
+++ app.js
@@ -9,69 +9,70 @@
 
 //Question 1
 
-function question1() {
-var toScor = 0;
-var userName = prompt('what\'s is your name?');
-
-alert('Hello ' + userName + '.  Thank you for visiting to my site!');
-
-
-var answerOne = prompt('Is Washington the \'Show Me State\'?');
-
-var myAnswerOne = 'no';
-
-if (answerOne === myAnswerOne) {
-  toScor += 1;
-  alert('You got it right, you rock!');
-
-} else {
-
-  alert('Sorry, that\'s wrong, try again!');
-
-}
-// // Question 2
-var answerTwo = prompt('Do you I collect comics as a hobby?');
-var myAnswerTwo = 'comics';
-if (answerTwo === myAnswerTwo) {
-  toScor += 1;
-  alert('Right again!');
-
-} else {
-
-  alert('Sorry, that\'s wrong, maybe next time!');
-}
-// Qustion 3
-var answerThree = prompt('Is the desert arid?').toLowerCase();
-var myAnswerThree = 'tan';
-if (answerThree === myAnswerThree) {
-
-  toScor += 1;
-  alert('You are a genious, right on!');
-
-} else {
-
-  alert('Oh no, that is incorrect.');
-}
-
-//Question 4
-var answerFour = prompt('Was Donkey Kong orignally called Jumpman?')
-var myAnswerFour = 'yes';
-if (answerFour === myAnswerFour) {
-
-  toScor += 1;
-  alert('Way to go, that\'s right!');
-
-
-} else {
-
-  alert('Oh no, that is incorrect.');
-}
-}
-
-question1();
+// function question1() {
+// var toScor = 0;
+// var userName = prompt('what\'s is your name?');
+//
+// alert('Hello ' + userName + '.  Thank you for visiting to my site!');
+//
+//
+// var answerOne = prompt('Is Washington the \'Show Me State\'?');
+//
+// var myAnswerOne = 'no';
+//
+// if (answerOne === myAnswerOne) {
+//   toScor += 1;
+//   alert('You got it right, you rock!');
+//
+// } else {
+//
+//   alert('Sorry, that\'s wrong, try again!');
+//
+// }
+// // // Question 2
+// var answerTwo = prompt('Do you I collect comics as a hobby?');
+// var myAnswerTwo = 'comics';
+// if (answerTwo === myAnswerTwo) {
+//   toScor += 1;
+//   alert('Right again!');
+//
+// } else {
+//
+//   alert('Sorry, that\'s wrong, maybe next time!');
+// }
+// // Qustion 3
+// var answerThree = prompt('Is the desert arid?').toLowerCase();
+// var myAnswerThree = 'tan';
+// if (answerThree === myAnswerThree) {
+//
+//   toScor += 1;
+//   alert('You are a genious, right on!');
+//
+// } else {
+//
+//   alert('Oh no, that is incorrect.');
+// }
+//
+// //Question 4
+// var answerFour = prompt('Was Donkey Kong orignally called Jumpman?')
+// var myAnswerFour = 'yes';
+// if (answerFour === myAnswerFour) {
+//
+//   toScor += 1;
+//   alert('Way to go, that\'s right!');
+//
+//
+// } else {
+//
+//   alert('Oh no, that is incorrect.');
+// }
+// }
+//
+// question1();
 
 // Question 5
-function questionTwo() {
+function questionTwo() { // wrapping set of questions in a function
+
 var whichLang = prompt('What is my favorite programming language?');
 var userAnswer = ['javascript', 'python', 'java', 'c'];
 
@@ -187,41 +188,42 @@ while (count < 6) {
 console.log(arr[i]);
 }
 questionTwo()
+
 //Keep score and show user results
-if (toScor === 8) {
-
-  alert('You got 8 of 8 correct, ' + userName + ' great going!');
-
-}if (toScor === 7) {
-
-  alert('You got 7 of 8 correct, ' + userName + ' strong effort!');
-
-}if (toScor === 6) {
-
-  alert('You got 6 of 8 correct, ' + userName + ' nice try.');
-
-}if (toScor === 5) {
-
-  alert('You got 5 of 8 correct, ' + userName + ' try again.');
-
-}if (toScor === 4) {
-
-  alert('You got 4 of 8 correct, ' + userName + ' try again.');
-
-}if (toScor === 3) {
-
-  alert('You got 3 of 8 correct, ' + userName + ' try again.');
-
-}if (toScor === 2) {
-
-  alert('You got 2 of 8 correct, ' + userName + ' try again.');
-
-}if (toScor === 1) {
-
-  alert('You got 1 of 8 correct, ' + userName + ' try again.');
-
-}if (toScor === 0) {
-
-  alert('You got 0 of 8 correct, ' + userName + ' try again.');
-}
-console.log(toScor);
+// if (toScor === 8) {
+//
+//   alert('You got 8 of 8 correct, ' + userName + ' great going!');
+//
+// }if (toScor === 7) {
+//
+//   alert('You got 7 of 8 correct, ' + userName + ' strong effort!');
+//
+// }if (toScor === 6) {
+//
+//   alert('You got 6 of 8 correct, ' + userName + ' nice try.');
+//
+// }if (toScor === 5) {
+//
+//   alert('You got 5 of 8 correct, ' + userName + ' try again.');
+//
+// }if (toScor === 4) {
+//
+//   alert('You got 4 of 8 correct, ' + userName + ' try again.');
+//
+// }if (toScor === 3) {
+//
+//   alert('You got 3 of 8 correct, ' + userName + ' try again.');
+//
+// }if (toScor === 2) {
+//
+//   alert('You got 2 of 8 correct, ' + userName + ' try again.');
+//
+// }if (toScor === 1) {
+//
+//   alert('You got 1 of 8 correct, ' + userName + ' try again.');
+//
+// }if (toScor === 0) {
+//
+//   alert('You got 0 of 8 correct, ' + userName + ' try again.');
+// }
+// console.log(toScor);
